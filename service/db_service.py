adab78550402d508babb0e1efca9d78fd007777c
AshanChandrasiri <ashanchandrasiri1@gmail.com>
2023-05-22 03:58:44 +0530

    update db connection class

--- service/db_service.py
+++ service/db_service.py
@@ -3,11 +3,14 @@ import boto3
 
 class DbService:
 
-    def __init__(self):
-        self.dynamodb_client = boto3.client("dynamodb")
+    def __init__(self, dynamodb_client=None):
+        if dynamodb_client is None:
+            self.dynamodb_client = boto3.client("dynamodb")
+        else:
+            self.dynamodb_client = dynamodb_client
 
     def save(self, table_name, item):
-        response = self.dynamodb_client.put_item(
+        self.dynamodb_client.put_item(
             TableName=table_name,
             Item=item
         )
